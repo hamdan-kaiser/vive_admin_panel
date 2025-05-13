83c20467ab08717de9f9ebe0cca830bbf0071dfe
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2025-05-13 23:25:44 +0600

    delete user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -364,7 +364,7 @@ class AuthController extends Controller
 
   public function deleteAccount(){
 
-    $data = User::where('id',9)->first();
+    $data = User::where('id',12)->first();
    
     if ($data == null){
         return response()->json([
